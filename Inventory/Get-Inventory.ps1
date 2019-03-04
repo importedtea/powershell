8a6042db776074201dfec256a59435684dd9c509
Matthew Fabrizio <mjffabrizio@gmail.com>
2019-03-04 18:28:40 -0500

    dynamic menu added

--- Inventory/Get-Inventory.ps1
+++ Inventory/Get-Inventory.ps1
@@ -1,91 +1,185 @@
-<#-------------------------
-    HTML Header & Style
---------------------------#>
-$Header = @"
-<center>
-    <style>
-        body { 
-            background-color:#E5E4E2;
-            font-family:Times New Roman;
-            font-size:12pt;
-            position:relative;
-        }
-        th{ 
-            border-width: 1px; padding: 0px; border-style: solid; border-color: black; background-color: #6495ED;
-        }
-        td {
-            border-width: 1px; padding: 0px; border-style: solid; border-color: black; width: 1px; white-space: nowrap;
-            height: 5px;
-            text-align: center;
-        }
-        table, tr, td, th { padding: 3px; margin: 0px ;white-space:pre; }
-        table { 
-            margin-left:auto;
-            margin-right:auto;
-            border-width: 2px; border-style: solid; border-color: black; border-collapse: collapse;
-        }
-        h2 {
-            text-align: center;
-            font-family:Tahoma;
-            color:#6D7B8D;
-        }
+<#
+.SYNOPSIS  
+Gets inventory of computers listed in various locations.
 
-        th[scope="col"] {
-            background: #ccc;
-        }
-        tr:nth-child(even) {
-            background: #eee;
-        }
-        tr:nth-child(odd) {
-            background: #ddd;
-        }
+.DESCRIPTION
+Generate quick and concise inventory with little manual maintenance. 
 
-        #footer {
-            color: black;
-            text-align:right;
-            position: static;
-            font-size: small;
-          }
-    </style>
-    <title>Computer Information Report</title>
-</center>
-"@
+.PARAMETER Simple
+Reduces the output for those who don't care about specifics.
 
-<#-------------------------
-    Menu Function
---------------------------#>
+.EXAMPLE
+Get-Inventory.ps1
+
+.EXAMPLE
+Get-Inventory.ps1 -Simple
+
+.NOTES
+Author: Matthew Fabrizio
+Version: 2.0.0
+
+________                                 _____       
+___  __ \____  ______________ _______ ______(_)______
+__  / / /_  / / /_  __ \  __ `/_  __ `__ \_  /_  ___/
+_  /_/ /_  /_/ /_  / / / /_/ /_  / / / / /  / / /__  
+/_____/ _\__, / /_/ /_/\__,_/ /_/ /_/ /_//_/  \___/  
+        /____/                                       
+
+Future Implementation:
+Get a list of computers from AD (priority = -1)
+
+.LINK
+    https://www.github.com/importedtea/powershell/Remote_Tools/
+#>
+
+param (
+    [Parameter(Mandatory=$false)]
+    [switch] $Simple
+)
+
 function Show-Menu {
-    Param (
-        [Parameter(Mandatory=$false, Position=0)]
-        [string] $Title
-    )
     Clear-Host
-    Write-Host "================ $Title ================"
-    Write-Host "1: Demo"
-    Write-Host "Q: Press 'Q' to quit."
-    Write-Host "================ $Title ================`n"
+
+    <#-------------------------
+        Print Menu Title
+    --------------------------#>
+    Write-Host "************ MENU ************"
+
+    <#-------------------------
+        Print Menu Entries
+    --------------------------#>
+    $Entry = 1
+    foreach ($Item in $List) {
+        Write-Host "[$Entry]: $($Item)"
+        $Entry++
+    }
+  
+    <#-------------------------
+        Add Static Entries
+    --------------------------#>
+    Write-Host "[Q]: Quit`n"
+      
+    <#-------------------------
+        Accept User Input
+    --------------------------#>
+    $Selection = Read-Host "Select a menu option"
+  
+    <#-------------------------
+        Validate STDIN
+    --------------------------#>
+    if ($Selection -match "^\d+$") {
+        # If one menu item, read that item
+        if ((Get-ChildItem "$PSScriptRoot\menu-entries.csv").Name.Count -eq 1) {
+            $Choice = $List
+        }
+        # If menu entry >=2
+        else {
+            # If Selection is numeric
+            $Choice = $($List[$Selection - 1])
+        }
+    }
+    else {
+        # If Selection is alpha
+        $Choice = $Selection
+    }
+
+    <#-------------------------
+        Return the Choice
+    --------------------------#>
+    return $Choice;
+}
+
+function Construct_Iventory() {
+    <#-------------------------
+        Create Root Directory
+    --------------------------#>
+    if (!(Test-Path "$PSScriptRoot\Classrooms\")) {
+        New-Item -Path . -ItemType Directory -Name "Classrooms" | Out-Null
+    }
+      
+    <#-------------------------
+        Create Menu Loop
+    --------------------------#>
+    foreach ($Classroom in $List) {
+        <#-------------------------
+            Initialize Helper Vars
+        --------------------------#>
+        $TEMP_DIR = $Classroom.ToLower().replace(" ", "_")
+        $TEMP_FILE = $Classroom.ToLower().replace(" ", "-") + '-computers' + '.txt'
+        $FULL_PATH = "$PSScriptRoot\Classrooms\$TEMP_DIR\$TEMP_FILE"
+          
+        <#-------------------------
+            Create Root\Directories
+        --------------------------#>
+        New-Item -Path "$PSScriptRoot\Classrooms\" -ItemType Directory -Name $TEMP_DIR -Force | Out-Null
+  
+        <#-------------------------
+            Generate computer.txt files
+        --------------------------#>
+        if (!(Test-Path $FULL_PATH)) {
+            $TEMP_RETVAL = $true  
+            New-Item -Path "$PSScriptRoot\Classrooms\$TEMP_DIR\" -ItemType File -Name $TEMP_FILE | Out-Null
+            Write-Host "You have added a new menu entry called $TEMP_FILE, please populate it now" -ForegroundColor Yellow; continue
+        }
+
+        <#-------------------------
+            Validate Content
+        --------------------------#>
+        if ($Null -eq (Get-Content $FULL_PATH)) {
+            $TEMP_RETVAL = $true
+            Write-Host "You are missing content for $TEMP_FILE, please populate it now." -ForegroundColor Yellow; continue
+        }
+    }
+    <#-------------------------
+        If NULL data; exit
+    --------------------------#>
+    if ($TEMP_RETVAL) { exit }
 }
 
 <#-------------------------
-    Generate Switch
+    Grab all menu entries
 --------------------------#>
-function Switch-Produce () {
-    Param(
-        [Parameter(Mandatory=$true, Position=0)]
-        [string] $Key,
-        [Parameter(Mandatory=$true, Position=1)]
-        [string] $Class,
-        [Parameter(Mandatory=$true, Position=2)]
-        [string] $ClassPC
-    )
-    $global:key = $key
-    $global:Class = $Class
-    $global:ClassPC = "${ClassPC}-computers.txt"
-
-    Write-Host "You chose $key" -ForegroundColor Yellow
-    Write-Host "The class directory is: $Class" -ForegroundColor Yellow
-    Write-Host "The computers list is: ${ClassPC}-computers.txt" -ForegroundColor Yellow
+if (!(Test-Path "$PSScriptRoot\menu-entries.csv")) { 
+    New-Item -Path "$PSScriptRoot\" -ItemType File -Name "menu-entries.csv" | Out-Null
+    Write-Host "You had no menu entries, please populate menu-entries.csv now" -ForegroundColor Yellow; exit
 }
+elseif ($Null -eq (Get-Content "$PSScriptRoot\menu-entries.csv")) {
+    Write-Host "You are missing content for menu-entries.csv, please populate it now." -ForegroundColor Yellow; exit
+}
+else { $List = Get-Content "$PSScriptRoot\menu-entries.csv" }
+  
+<#-------------------------
+    Grab HTML Styling
+--------------------------#>
+$Header = Get-Content "$PSScriptRoot\assets\style.html"
+
+<#-------------------------
+    Call Construct_Iventory
+    Generate/Validate data
+--------------------------#>
+Construct_Iventory
+
+<#-------------------------
+    Menu Selection Logic
+--------------------------#>
+do {
+    $Choice = Show-Menu
+
+    switch ($Choice) {
+        { $List -contains $Choice } {
+            $Key = $Choice
+            $Shop = $Choice.ToLower().replace(" ", "-")
+            $ShopPC = "$Choice-computers.txt".ToLower().Replace(" ", "-")
+
+            # Match if $list array contains the $selected $Choice object
+            Write-Host "`nYou chose $($Choice)." -ForegroundColor Green
+            Write-Host "Inventory will be pulled from $Shop\$ShopPC`n" -ForegroundColor Green
+        }
+        'q' { exit }
+        '\' { Write-Host "dev setting" -ForegroundColor Green }
+        default { Write-Host "Invalid menu choice" -ForegroundColor Red }  
+    }
+} while ($Choice -eq 'q')
 
 <#-------------------------
     Error Preference
@@ -98,20 +192,6 @@ $ErrorActionPreference = 'Stop'
 $Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 
 <#-------------------------
-        Menu Loop
---------------------------#>
-do {
-    Show-Menu -Title "Classrooms"
-    $Choice = Read-Host "What classroom would you like to inventory?"
-
-    switch ($Choice) {
-        1 { Switch-Produce "Demo" demo demo }
-        'q' { exit }
-        default { Show-Menu }
-    }
-} while ($Choice -eq 'q')
-
-<#-------------------------
     Array Creation
 --------------------------#>
 $Results = @()
@@ -119,13 +199,13 @@ $Results = @()
 <#-------------------------
         Computers
 --------------------------#>
-$Computers = (Get-Content $PSScriptRoot\Classrooms\$Class\$ClassPC)
+$Computers = (Get-Content "$PSScriptRoot\Classrooms\$Shop\$ShopPC")
 
 <#-------------------------
     Directory/File Creation
 --------------------------#>
 $Filename = Read-Host "What would you like your filename to be? "
-$Dirname = New-Item -Path "$PSScriptRoot\Classrooms\$Class\" -Name "inv" -ItemType Directory -Force
+$Dirname = New-Item -Path "$PSScriptRoot\Classrooms\$Shop\" -Name "inv" -ItemType Directory -Force
 
 <#-------------------------
     $Computer Loop
@@ -144,7 +224,7 @@ ForEach ($computer in $Computers) {
             OS = (Get-WmiObject -ComputerName $computer -Class Win32_OperatingSystem).Version
             Memory = Get-WmiObject -ComputerName $computer -Class Win32_ComputerSystem | Select-Object TotalPhysicalMemory, @{name="GB";expr={[float]($_.TotalPhysicalMemory/1GB)}} | Select-Object -ExpandProperty GB
             IP = ([System.Net.Dns]::GetHostByName("$computer").AddressList[0]).IpAddressToString
-            Domain = (Get-WmiObject -ComputerName $computer -Class win32_computersystem).Domain
+            Domain = (Get-WmiObject -ComputerName $computer -Class Win32_Computersystem).Domain
             MAC = (Get-WmiObject -ComputerName $computer -Class Win32_NetworkAdapterConfiguration -Filter "IPEnabled='True'").MACAddress | Select-Object -First 1
             Age = Get-WmiObject -ComputerName $computer -Class Win32_BIOS
             ReimageDate = Get-WmiObject -ComputerName $computer -Class Win32_OperatingSystem
@@ -166,9 +246,9 @@ ForEach ($computer in $Computers) {
         <#-------------------------
                 Calculations
         --------------------------#>
-        $Properties.Age = (New-TimeSpan -Start ($Properties.Age.ConvertToDateTime($Properties.Age.releasedate).ToShortDateString()) -End $(Get-Date)).Days / 365
+        $Properties.Age = (New-TimeSpan -Start ($Properties.Age.ConvertToDateTime($Properties.Age.ReleaseDate).ToShortDateString()) -End $(Get-Date)).Days / 365
         $Properties.ReimageDate = ($Properties.ReimageDate.ConvertToDateTime($Properties.ReimageDate.InstallDate).ToString("MM-dd-yyyy"))
-
+        
         <#-------------------------
             Spicy Console Log
         --------------------------#>
@@ -204,18 +284,32 @@ ForEach ($computer in $Computers) {
 <#-------------------------
         CSV Export
 --------------------------#>
-$Results | Select-Object Hostname,Manufacturer,Model,Serial,Edition,OS,Memory,IP,MAC,Domain,Age,ReimageDate | Export-Csv -Path $Dirname\${Filename}_$((Get-Date).ToString('MM-dd-yyyy')).csv -NoTypeInformation
+if ($Simple) {
+    $Results | Select-Object Hostname,Manufacturer,Model,Serial,Edition,OS | Export-Csv -Path $Dirname\${Filename}_$((Get-Date).ToString('MM-dd-yyyy')).csv -NoTypeInformation
+}
+else {
+    $Results | Select-Object Hostname,Manufacturer,Model,Serial,Edition,OS,Memory,IP,MAC,Domain,Age,ReimageDate | Export-Csv -Path $Dirname\${Filename}_$((Get-Date).ToString('MM-dd-yyyy')).csv -NoTypeInformation
+}
 
 <#-------------------------
         HTML Export
 --------------------------#>
-$PCTitle = "<h2>Computer Information Report for $Key</h2>"
+# $PCTitle = "<h2>Computer Information Report for $Key</h2>"
+$PCTitle = "<div class='header'>Computer Information Report for $Key</div>"
 
-## Create the HTML file
-$Results | Select-Object Hostname,Manufacturer,Model,Serial,Edition,OS,Memory,IP,MAC,Domain,Age,ReimageDate | ConvertTo-Html -Head $Header -Body $PCTitle | Out-File $Dirname\${Filename}_$((Get-Date).ToString('MM-dd-yyyy')).html
+# Create the HTML file
+if ($Simple) {
+    $Results | Select-Object Hostname,Manufacturer,Model,Serial,Edition,OS | ConvertTo-Html -Head $Header -Body $PCTitle | Out-File $Dirname\${Filename}_$((Get-Date).ToString('MM-dd-yyyy')).html
+
+}
+else {
+    $Results | Select-Object Hostname,Manufacturer,Model,Serial,Edition,OS,Memory,IP,MAC,Domain,Age,ReimageDate | ConvertTo-Html -Head $Header -Body $PCTitle | Out-File $Dirname\${Filename}_$((Get-Date).ToString('MM-dd-yyyy')).html
+ 
+}
 
 ## Append a footer
-$footer = "<footer id='footer'><i>$(get-date -Format g)</i></footer>"
+# $footer = "<footer id='footer'><i>$(get-date -Format g)</i></footer>"
+$footer = "<div class='Copyright'>$((Get-Date -Format g))</div>"
 $footer | Out-File $Dirname\${Filename}_$((Get-Date).ToString('MM-dd-yyyy')).html -Append
 
 <#-------------------------
@@ -229,4 +323,4 @@ Write-Host "You can view the HTML report at $Dirname\${Filename}_$((Get-Date).To
 --------------------------#>
 $Stopwatch.Stop()
 $Time = $Stopwatch.Elapsed
-Write-Host "The script took $Time seconds`n"
+Write-Host "`nThe script took $Time seconds`n"
